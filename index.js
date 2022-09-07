56940ad9fdf281aab76d26eaf19e1e2c7069cd3f
Coder_G <86252632+Harsh115-tech@users.noreply.github.com>
2022-09-07 12:43:26 +0530

    jquery validation
    add validation to main add user form

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 $("#add_user_form").validate({
     rules:{
-        fname:"required"
+        fname: {
+            required: true,
+            minlength: 2
+        },
+        lname: {
+            required: true,
+            minlength:2
+        },
+        stream: {
+            required: true,
+            minlength:1
+        }
+
     },messages:{
-        fname:"Please Enter Your Name"
+        fname:{
+            required : "Please Enter Your First Name",
+            minlength : "Your Email Must Be At least 5 Characters Long"
+        },
+        lname:{
+            required : "Please Enter Your Last Name",
+            minlength : "Your password Must Be At least 5 Characters Long"
+        },
+        stream:{
+            required : "Please Enter Your Password",
+            minlength : "Your Password Must Be At least 5 Characters Long"
+        },
     }   
 });
